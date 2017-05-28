3b6e58a8cf99ef1fc68fb4c93a0bfc1f0e661811
rvnlvrz <rvnlvrz@gmail.com>
2017-05-28 18:58:53 +0800

    added file open and save dialogs

--- OfficeEquipMgmtApp/OfficeEquipMgmtApp/Main Form.cs
+++ OfficeEquipMgmtApp/OfficeEquipMgmtApp/Main Form.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using EquipmentLibrary;
 namespace OfficeEquipMgmtApp
 {
     public partial class Main : Form
@@ -33,6 +25,33 @@ namespace OfficeEquipMgmtApp
         private void updateTime(object sender, EventArgs e)
         {
             striplbl.Text = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //File_Browser browser = new File_Browser();
+            //browser.ShowDialog();
+
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "XML Files|*.xml|Database Files|*.db";           
+            open.Title = "Open Inventory File";
+
+            if (open.ShowDialog() == DialogResult.OK) // if the user pressed OK on the form then read the file
+            {
+                this.Cursor = new Cursor(open.OpenFile());
+            }
+          
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "XML Files|*.xml|Database Files|*.db";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                this.Cursor = new Cursor(save.OpenFile());
+            }
         }
     }
 }
